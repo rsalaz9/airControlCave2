1f271dd98141cf47bbf848164b6ed6c313bb3f95
Nafiul Nipu <nafiulnipu.2013@gmail.com>
2020-03-26 13:19:36 -0500

    path

---
+++ AirControlCave2/Assets/PathGenerateAuto.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathGenerateAuto : MonoBehaviour
+{
+    public Transform [] target;
+    public float speed;
+
+    public int current;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position != target[current].position){
+            Vector3 pos =  Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+         }else{ //} if(current < (target.Length - 1)){
+            current = (current + 1) % target.Length;
+        }
+        // }else if(current = (target.Length - 1)){
+        //     continue;
+        // }
+        
+    }
+}
