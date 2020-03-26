48a608728444d82d540becc02f2287ae523103c8
Nafiul Nipu <nafiulnipu.2013@gmail.com>
2020-03-26 15:14:45 -0500

    pos to push to terminal

--- AirControlCave2/Assets/PathGenerateAuto.cs
+++ AirControlCave2/Assets/PathGenerateAuto.cs
@@ -4,23 +4,55 @@ using UnityEngine;
 
 public class PathGenerateAuto : MonoBehaviour
 {
-    public Transform [] target;
-    public float speed;
+    public Transform [] target; //target objects i.e. push, terminals
+    public int p; //for now acting as the menu button
+    public float speed; // speed of our plane
 
-    public int current;
+    public int current; //index of the target array
+
+    //boolean to make sure update runs once only
+    bool PushpathGen = true; 
+    bool TerminalpathGen = true;
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position != target[current].position){
-            Vector3 pos =  Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
-            GetComponent<Rigidbody>().MovePosition(pos);
-         }else{ //} if(current < (target.Length - 1)){
-            current = (current + 1) % target.Length;
+        //push will work first 
+        if(PushpathGen){
+            PushpathGenerator(); //method for push
+        }else{
+            //then we will move to the terminal
+            if(TerminalpathGen){
+                TerminalpathGenerator(p); //method for terminal
+            }
         }
-        // }else if(current = (target.Length - 1)){
-        //     continue;
+
+    }
+
+    public void PushpathGenerator(){
+            
+                if(transform.position != target[current].position){
+                    Vector3 pos =  Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+                    GetComponent<Rigidbody>().MovePosition(pos);
+                }else if(transform.position == target[current].position){
+                    PushpathGen = false;
+                }
+
+    }
+
+    public void TerminalpathGenerator(int targetTerminal){
+            current = targetTerminal;            
+            if(transform.position != target[current].position){
+                Vector3 pos =  Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+                GetComponent<Rigidbody>().MovePosition(pos);
+            }else if(transform.position == target[current].position){
+                TerminalpathGen = false;
+            }
+
+    }
+        //make a loop of path
+        // else{ 
+        //     current = (current + 1) % target.Length;
         // }
         
-    }
 }
