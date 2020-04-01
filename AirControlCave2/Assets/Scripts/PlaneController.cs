fd48056ccfad35ceee457188c67bff7ccd4619df
carla_floricel <carla.floricel@gmail.com>
2020-03-31 22:10:09 -0400

    score added

---
+++ AirControlCave2/Assets/Scripts/PlaneController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Score scoreManager;
+
+
+        void OnCollisionEnter(Collision theCollision)
+    {
+        if(theCollision.gameObject.tag == "TakeOffATarget" || theCollision.gameObject.tag == "TakeOffBTarget" ){
+                Debug.Log("collision detected");
+                scoreManager.AddPoint();
+                Destroy(gameObject);
+                
+        }
+    }
+}
