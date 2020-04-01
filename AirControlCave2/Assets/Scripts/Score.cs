f49b2962d1c12dea2697be0693d8fa1d77c35e47
carla_floricel <carla.floricel@gmail.com>
2020-04-01 00:36:00 -0400

    score added

---
+++ AirControlCave2/Assets/Scripts/Score.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Score : MonoBehaviour
+{
+    public Text scoreText;
+    int score;
+    void Start()
+    {
+        score = 0;
+        scoreText.text = "Score: " + score;
+       
+    }
+
+    public void AddPoint()
+    {
+        Debug.Log("point added");
+        score++;
+        scoreText.text = "Score: " + score;
+
+    }
+
+}
