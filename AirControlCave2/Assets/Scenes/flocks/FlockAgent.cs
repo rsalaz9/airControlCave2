86763cc54647b8b759c5967d8389edb73c5cdd16
carla_floricel <carla.floricel@gmail.com>
2020-04-03 19:15:00 -0400

    buildings added

---
+++ AirControlCave2/Assets/Scenes/flocks/FlockAgent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class FlockAgent : MonoBehaviour
+{
+    SphereCollider agentCollider;
+    public SphereCollider AgentCollider{ get {return agentCollider;}}
+    // Start is called before the first frame update
+    void Start()
+    {
+        agentCollider = GetComponent<SphereCollider>();
+    }
+
+    public void Move(Vector3 velocity){
+        transform.up = velocity;
+        transform.position += (Vector3)velocity * Time.deltaTime;
+    }
+}
