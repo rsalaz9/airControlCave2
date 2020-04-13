8cd82121b10c7c01eea8c35867550e1723dd3b14
carla_floricel <carla.floricel@gmail.com>
2020-04-13 13:50:23 -0400

    dummy menu

---
+++ AirControlCave2/Assets/Scenes/Scripts/MenuButtons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ButtonStart(){
+        SceneManager.LoadScene(1);
+    }
+
+    public void ButtonOptions(){
+        SceneManager.LoadScene(2);
+    }
+
+    public void ButtonExit(){
+        Application.Quit();
+    }
+
+    public void ButtonMenu(){
+        SceneManager.LoadScene(0);
+    }
+}
